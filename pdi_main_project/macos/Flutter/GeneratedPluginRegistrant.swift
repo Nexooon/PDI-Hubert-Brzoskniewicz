e690e45dd9110c6e6b1a78bdfb581f76ae6b0b8d
Hubert <hubert.brzoskniewicz@gmail.com>
2025-05-19 21:42:02 +0200

    desktop not compatible

--- pdi_main_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ pdi_main_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,12 +7,22 @@ import Foundation
 
 import cloud_firestore
 import cloud_functions
+import file_picker
 import firebase_auth
 import firebase_core
+import firebase_storage
+import open_file_mac
+import path_provider_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
-  FLTFirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFunctionsPlugin"))
+  FirebaseFunctionsPlugin.register(with: registry.registrar(forPlugin: "FirebaseFunctionsPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
